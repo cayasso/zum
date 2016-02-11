554efda9daf0b8377bda97226793c7350ab2a9fa
cayasso <cayasso@gmail.com>
2016-02-11 14:59:09 -0600

    [minor] adding listening event on server

--- examples/server.js
+++ examples/server.js
@@ -5,3 +5,22 @@ var zum = require('../lib/index');
 var bus = zum();
 
 bus = bus.bind();
+
+/**
+ * Handle errors.
+ */
+
+bus.on('error', (err) => {
+  console.error(err);
+});
+
+/**
+ * Announce connection.
+ */
+
+bus.on('listening', (info) => {
+  console.log('-----------------------------------------------------');
+  console.log(' CONNECTED : ' + info.host);
+  console.log(' STARTED   : ' + new Date());
+  console.log('-----------------------------------------------------');
+});
